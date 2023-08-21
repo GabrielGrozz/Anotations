baaf83fd979249a6c4c9c5021e1ea88003bcbe37
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-21 10:51:12 -0300

    maked the project

---
+++ Anotations/Anotations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Anotations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
