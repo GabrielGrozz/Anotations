b4939a27fb9941ee24b31c23a251eb59b55090f5
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-21 13:09:41 -0300

    dbContext configured -before crate database

---
+++ Anotations/Anotations/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Anotations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anotations.Context
+{
+    public class AppDbContext : DbContext
+    {
+        //construtor que irá carregar as informações necessárias configurar o DbContext
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        DbSet<Anotation> Anotations { get; set; }
+    }
+}
